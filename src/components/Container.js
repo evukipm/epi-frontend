161b4a334b57fde8eb77e0c4bdb8485d8fcfc848
Iban Dasca <ibandasca@MacBook-Pro-de-Iban.local>
2018-11-10 19:21:54 +0100

    Container enhancement

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
  class Container extends Component {
+  
+
+  increaseVote = (id) => {
+    console.log(id);
+  }
+
+  decreaseVote = () => {
+    console.log('-1');
+  }
+
   render() {
     const { data } =this.props
     return (
@@ -10,10 +20,17 @@ import React, { Component } from 'react'
           <p>Author: {data.author.username}</p>
           <p>{data.text}</p>
           <ol>
-          {data.steps.map(step => {
-            return <li>
-              <div>{step.votes.positive}</div>
-              <div>{step.votes.negative}</div>
+          {data.steps.map((step, key) => {
+
+            return <li key={key}>
+              <div>
+                <div>{step.votes.positive}</div>
+                <img src={`${process.env.PUBLIC_URL}/img/arrow_up.png`} alt="arrow-img" width="40px" onClick={() => this.increaseVote(step._id)} ></img>
+              </div>
+              <div>
+                <div>{step.votes.negative}</div>
+                <img src={`${process.env.PUBLIC_URL}/img/arrow_down.png`} alt="arrow-img" width="40px" onClick={this.decreaseVote}></img>
+              </div>
               <div>{step.step}</div>            
             </li>
           })}
